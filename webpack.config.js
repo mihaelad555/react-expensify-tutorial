7e09442b6eb588a93932b09c9234485bd0162d37
mihaelad <mihaelad@cisco.com>
2018-12-04 12:24:55 -0500

    chandes styel

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,53 @@
-const path =require('path');
-//entry point - output
-//console.log(path.join(__dirname,'public'));
-module.exports ={
-    entry:'./src/app.js',
-    mode: 'development',
-    output: {
-        path: '/Users/mihaelad/udemytotorials/expensify-app/public', ////path.join/(__dirname,'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude:/node_modules/
-        },
-    {
-        test:/\.s?css$/,
-        use:[
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-        ] 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+module.exports = (env) => {
+
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    console.log(env);
+    //entry point - output
+    //console.log(path.join(__dirname,'public'));
+    return {
+        entry: './src/app.js',
+        mode: 'development',
+        output: {
+            path: '/Users/mihaelad/udemytotorials/expensify-app/public', ////path.join/(__dirname,'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: CSSExtract.extract({
+                        use: [{
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            }, {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+
+                        ]
+                    })
+
+                }
+            ]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: '/Users/mihaelad/udemytotorials/expensify-app/public',
+            historyApiFallback: true
+        }
     }
-    ]
-    },
-    devtool:'cheap-module-eval-source-map',
-     devServer: {
-        contentBase: '/Users/mihaelad/udemytotorials/expensify-app/public',
-        historyApiFallback:true
- }
 };
